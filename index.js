155f3c9e5a0cb9457433765bee7e7b3179bd48fe
Shahjahan Md Swajan <swajan.mis13.du@gmail.com>
2020-10-14 12:51:44 +0600

    GET and POST api is ready for services and reviews

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
 const morgan = require("morgan");
+const fileUpload = require('express-fileupload')
 require("dotenv").config();
 
 const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('services'));
+app.use(fileUpload())
+
 const port = 4000;
 
 const url = process.env.DB_URL;
@@ -18,6 +22,45 @@ const dbname = "creativeAgency";
 MongoClient.connect(url, (err, client) => {
     console.log("connected db");
     const db = client.db(dbname);
+    const servicesCollection = db.collection("services");
+    const ordersCollection = db.collection("orders");
+    const reviewsCollection = db.collection("reviews");
+    // post services
+    app.post("/add-service", (req, res) => {
+        const icon = req.files.icon;
+        const title = req.body.title;
+        const description = req.body.description;
+        icon.mv(`${__dirname}/services/${icon.name}`, err => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send({ msg: "Failed to upload image" })
+            }
+            servicesCollection.insertOne({ icon: icon.name, title, description }).then(
+                result => {
+                    res.send(result.insertedCount > 0)
+                }
+            )
+        })
+    })
+    // add review
+    app.post("/add-review", (req, res) => {
+        const newReview = req.body;
+        reviewsCollection.insertOne(newReview).then(result => {
+            res.send(result.insertedCount > 0)
+        })
+    })
+
+    // Get Requests
+    app.get('/services', (req, res) => {
+        servicesCollection.find({}).toArray((err, documents) => {
+            res.send(documents)
+        })
+    })
+    app.get('/reviews', (req, res) => {
+        reviewsCollection.find({}).toArray((err, documents) => {
+            res.send(documents)
+        })
+    })
 
 });
 
